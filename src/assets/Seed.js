cd3cf8f68e8e8ff3118d5a75c060d7ffff261b0f
Charles Butler <cdbutle@outlook.com>
2021-03-12 14:10:52 -0600

    added TL seeds

---
+++ src/assets/Seed.js
@@ -0,0 +1,52 @@
+let tlSeeds = [{
+        id: 1,
+        title: "First Entry",
+        location: "City, State",
+        description: "Description of the first entry",
+        buttonText: "webpage",
+        date: "Month Year - present",
+        icon: "school",
+    },
+
+    {
+        id: 2,
+        title: "2nd Entry",
+        location: "City, State",
+        description: "Description of the 2nd entry",
+        buttonText: "webpage",
+        date: "Month Year - present",
+        icon: "work",
+    },
+
+    {
+        id: 3,
+        title: "3rd Entry",
+        location: "City, State",
+        description: "Description of the 3rd entry",
+        buttonText: "webpage",
+        date: "Month Year - present",
+        icon: "travel",
+    },
+
+    {
+        id: 4,
+        title: "4th Entry",
+        location: "City, State",
+        description: "Description of the 4th entry",
+        buttonText: "webpage",
+        date: "Month Year - present",
+        icon: "work",
+    },
+
+    {
+        id: 5,
+        title: "3rd Entry",
+        location: "City, State",
+        description: "Description of the 3rd entry",
+        buttonText: "webpage",
+        date: "Month Year - present",
+        icon: "video",
+    },
+];
+
+export default tlSeeds;
