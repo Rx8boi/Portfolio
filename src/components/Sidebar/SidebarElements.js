c6dd00513d8c24bc68c90ac981c0e6c474a2beb1
Charles Butler <cdbutle@outlook.com>
2021-03-15 11:49:11 -0500

    sidebar completed

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -16,7 +16,8 @@ export const SidebarContainer = styled.aside`
   transition: 0.3s ease-in-out;
   opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
   top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
-  
+  /* testing mobile navbar */
+  /* top: 0; */
 `;
 export const CloseIcon = styled(FaTimes)`
   color: #fff;
