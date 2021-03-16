4f88099a25d33fc25b449ae21ac931684ccfa56f
Charles Butler <cdbutle@outlook.com>
2021-03-16 15:38:57 -0500

    Page styling

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button } from '../ButtonElement'
-
+import { InfoContainer, InfoWrapper, InfoRow, Column1, Column2, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, ImgWrap, Img } from './InfoElements'
 
 
 const InfoSection = () => {
   return (
     <>
-      <InfoContainer id={id}>
+      <InfoContainer>
         <InfoWrapper>
           <InfoRow>
             <Column1>
@@ -23,7 +23,11 @@ const InfoSection = () => {
                   </Heading>
                 </TopLine>
               </TextWrapper>
-            </Column1>
+                      </Column1>
+                      <Column2>
+                          <ImgWrap>
+                              <Img />
+                          </ImgWrap></Column2>
           </InfoRow>
         </InfoWrapper>
       </InfoContainer>
