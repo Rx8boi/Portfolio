af9f85ce5bcb06c02f48ab1c94cebc3b65201ee3
Charles Butler <cdbutle@outlook.com>
2021-03-26 12:10:54 -0500

    projects template

---
+++ src/components/Projects/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+      <div>
+        <ProjectsContainer id="Projects">
+          <ProjectsH1>Projects</ProjectsH1>
+          <ProjectsWrapper>
+            <ProjectsCard>
+              <ProjectsIcon src={Icon1} />
+              <ProjectsH2> Video Game Tracker CLI</ProjectsH2>
+              <ProjectsP>Summary: Ruby based project with fetch calls</ProjectsP>
+              {/* <ProjectsSubtitle darkText={darkText}> {description}</ProjectsSubtitle> */}
+            </ProjectsCard>
+            <ProjectsCard>
+              <ProjectsIcon src={Icon2} />
+              <ProjectsH2> Marvel Movie Project</ProjectsH2>
+              <ProjectsP>Summary: Utilized Sinatra gem.</ProjectsP>
+              {/* <ProjectsSubtitle darkText={darkText}> {description}</ProjectsSubtitle> */}
+            </ProjectsCard>
+            <ProjectsCard>
+              <ProjectsIcon src={Icon3} />
+              <ProjectsH2> GameRateSwap</ProjectsH2>
+              <ProjectsP>
+                Summary: GameRateSwap application that utilizes rails framework,
+                omniauth gem & Devise.
+              </ProjectsP>
+              {/* <ProjectsSubtitle darkText={darkText}> {description}</ProjectsSubtitle> */}
+            </ProjectsCard>
+            <ProjectsCard>
+              <ProjectsIcon src={Icon4} />
+              <ProjectsH2> My Fantasy League: NBA</ProjectsH2>
+              <ProjectsP>
+                Summary: Fantasy League for when you're ready for game time. NBA
+                support with future NFL updates.
+              </ProjectsP>
+              {/* <ProjectsSubtitle darkText={darkText}> {description}</ProjectsSubtitle> */}
+            </ProjectsCard>
+            <ProjectsCard>
+              <ProjectsIcon src={Icon5} />
+              <ProjectsH2> A Good Calculator</ProjectsH2>
+              <ProjectsP>
+                Summary: Simple calculator utilizing AngularJS. Showing junior level software engineering qualities.
+              </ProjectsP>
+              {/* <ProjectsSubtitle darkText={darkText}> {description}</ProjectsSubtitle> */}
+            </ProjectsCard>
+            <ProjectsCard>
+              <ProjectsIcon src={Icon6} />
+              <ProjectsH2> TimeLine</ProjectsH2>
+              <ProjectsP>
+                Summary: TIme-Line application for Flatiron Final Project
+              </ProjectsP>
+              {/* <ProjectsSubtitle darkText={darkText}> {description}</ProjectsSubtitle> */}
+            </ProjectsCard>
+          </ProjectsWrapper>
+        </ProjectsContainer>
+      </div>
+    );
+}
+
+export default Projects
