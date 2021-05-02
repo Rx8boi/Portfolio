eef45617aa7d50b10a7bd13042b822e6ca5d38f5
Charles Butler <cdbutle@outlook.com>
2021-05-01 20:45:18 -0500

    code cleanup

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -13,6 +13,8 @@ import {
   Img,
 } from "./InfoElements";
 
+
+
 const InfoSection = ({
   lightBg,
   id,
@@ -35,22 +37,6 @@ const InfoSection = ({
                 <TopLine>{topLine}</TopLine>
                 <Heading lightText={lightText}>{headline} </Heading>
                 <Subtitle darkText={darkText}> {description}</Subtitle>
-                {/* <BtnWrap>
-                  <Button
-                    //   to="home"
-                    to="TimeLine"
-                    smooth={true}
-                    duration={500}
-                    spy={true}
-                    exact="true"
-                    offset={-80}
-                    primary={primary ? 1 : 0}
-                    dark={dark ? 1 : 0}
-                    dark2={dark2 ? 1 : 0}
-                  >
-                    {buttonLabel}
-                  </Button>
-                </BtnWrap> */}
               </TextWrapper>
             </Column1>
             <Column2>
