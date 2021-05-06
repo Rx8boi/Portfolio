65bbe30e60cde1af953ebc1d215982934f2ce3ef
Charles Butler <cdbutle@outlook.com>
2021-05-05 20:40:28 -0500

    seeded languages data

---
+++ src/components/Contact/data.js
@@ -0,0 +1,72 @@
+let ContactLang = [
+  {
+    id: 1,
+        icon: require("../../assets/languages/ruby.svg"),
+    
+  },
+  {
+    id: 2,
+    icon: require("../../assets/languages/sinatra.svg"),
+  },
+  {
+    id: 3,
+    icon: require("../../assets/languages/rails-1.svg"),
+  },
+  {
+    id: 4,
+    icon: require("../../assets/languages/php.svg"),
+  },
+  {
+    id: 5,
+    icon: require("../../assets/languages/logo-javascript.svg"),
+  },
+  { id: 6, icon: require("../../assets/languages/angular.svg") },
+  {
+    id: 7,
+    icon: require("../../assets/languages/icons8-react.svg"),
+  },
+  { id: 8, icon: require("../../assets/languages/redux.svg") },
+  {
+    id: 9,
+    icon: require("../../assets/languages/icons8-nodejs.svg"),
+  },
+  {
+    id: 10,
+    icon: require("../../assets/languages/icons8-postgresql.svg"),
+  },
+
+  {
+    id: 12,
+    icon: require("../../assets/languages/sql.png"),
+  },
+  {
+    id: 13,
+    icon: require("../../assets/languages/icons8-mongodb.svg"),
+  },
+  {
+    id: 14,
+    icon: require("../../assets/languages/c-logo.svg"),
+  },
+  {
+    id: 15,
+    icon: require("../../assets/languages/icons8-python.svg"),
+  },
+  {
+    id: 16,
+    icon: require("../../assets/languages/html5.svg"),
+  },
+  {
+    id: 17,
+    icon: require("../../assets/languages/css.svg"),
+  },
+  {
+    id: 18,
+    icon: require("../../assets/languages/cpanel.svg"),
+  },
+  {
+    id: 19,
+    icon: require("../../assets/languages/git-icon.png"),
+  },
+];
+
+export default ContactLang;
